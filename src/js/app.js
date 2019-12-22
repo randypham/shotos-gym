bb5fc2ad1e482cd38e56129a916765247c54b0cb
Randy Pham <randypham50@gmail.com>
2019-12-21 21:05:44 -0500

    Finished hero and added header scroll

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-/* Navigation */
+/* Mobile Navigation Toggle  */
 const headerNav = document.querySelector('#header-nav');
 const headerNavBtn = document.querySelector('#header-nav-btn');
 
@@ -7,3 +7,16 @@ function toggleNav() {
 }
 
 headerNavBtn.addEventListener('click', toggleNav);
+
+/* Header Background Scroll Change */
+const header = document.querySelector('#header');
+
+function headerBackgroudChange() {
+  if (window.scrollY > 100) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+}
+
+window.addEventListener('scroll', headerBackgroudChange);
